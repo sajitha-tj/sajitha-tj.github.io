e5e4fab2e677a93a3d291c4bf794edf689b4d780
sajitha-tj <sajithatj001@gmail.com>
2024-08-12 19:52:19 +0530

    initial commit

---
+++ components/Hero.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { BackgroundBeams } from './ui/background-beams'
+import { FaBehance, FaGithub, FaLinkedin } from 'react-icons/fa6'
+import { LuMail } from 'react-icons/lu'
+
+const Hero = () => {
+  return (
+    <div className='w-full h-screen bg-black text-white pt-36 md:pl-40 pb-20 overflow-hidden'>
+        <div className='pt-[22vh] md:pt-36 flex flex-col items-center md:items-start relative z-10'>
+            <div className='text-5xl md:text-6xl leading-tight md:leading-snug tracking-wide text-center md:text-left font-bold'>
+                <h1>Hey There! I&#39;m</h1>
+                <h1 className='text-violet-200'>Sajitha Jayawickrama</h1>
+            </div>
+            <div>
+                <p className='uppercase tracking-widest text-xs mt-3 text-center md:text-left leading-snug'>
+                    CyberSecurity Enthusiast <span className='hidden md:inline'>|</span> <br className='inline md:hidden' /> Developer | Designer
+                </p>
+            </div>
+            {/* links */}
+            <div className='w-full flex justify-center md:justify-start'>
+            <div className='mt-3 text-lg w-fit flex gap-4'>
+                <a href='https://github.com/sajitha-tj/' className='transition-colors hover:text-violet-400 duration-100' target='_blank'>
+                    <FaGithub />
+                </a>
+                <a href='https://www.linkedin.com/in/sajitha-tj/' className='transition-colors hover:text-violet-400 duration-100' target='_blank'>
+                    <FaLinkedin />
+                </a>
+                <a href='https://www.behance.net/sajitha_jayawickrama' className='transition-colors hover:text-violet-400 duration-100' target='_blank'>
+                    <FaBehance />
+                </a>
+                <a href='mailto:sajitha.21@cse.mrt.ac.lk' className='transition-colors hover:text-violet-400 duration-100' target='_blank'>
+                    <LuMail />
+                </a>
+            </div>
+        </div>
+        </div>
+        <BackgroundBeams />
+    </div>
+  )
+}
+
+export default Hero
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,10 @@
 import type { Config } from "tailwindcss";
 
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+ 
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,13 +13,23 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily:{
+        
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 };
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
+
 export default config;
