3e48d3d3dc1aa1d753570c824b1a429b81eea4d0
sajitha-tj <sajithatj001@gmail.com>
2024-08-13 13:38:22 +0530

    added 404 page and resume section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
+import Resume from "@/components/Resume";
 
 export default function Home() {
   return (
     <main className="relative mx-auto">
       <Hero />
+      <div className="md:px-40 px-10">
+        <Resume />
+        <Projects />
+      </div>
     </main>
   );
 }
---
+++ components/Projects.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div className='py-20'>
+        <h2 className='text-3xl font-bold border-l-4 pl-4'>Thanks for Droppig by ❤️ <br /><span className='font-normal text-lg'>This site is still under work..</span></h2>
+    </div>
+  )
+}
+
+export default Projects
